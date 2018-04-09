25f93aec3ebcc10b44e5737fd4f3356ce0b16645
r17171709 <Yuren2@hotmail.com>
2018-04-09 09:14:34 +0800

    调整到房博士版本下

--- app/src/main/java/com/renyu/mt/activity/SignInActivity.java
+++ app/src/main/java/com/renyu/mt/activity/SignInActivity.java
@@ -51,7 +51,8 @@ public class SignInActivity extends BaseIMActivity {
                         ACache.get(Utils.getApp()).put("UserInfoRsp", userInfoRsp);
                         // 用户登录信息
                         SPUtils.getInstance().put(CommonParams.SP_UNAME, ed_username.getText().toString());
-                        SPUtils.getInstance().put(CommonParams.SP_PWD, GostDesBasae64Encrypter.encode(ed_username.getText().toString(), FusionField.key32));
+                        SPUtils.getInstance().put(CommonParams.SP_PWD, ed_pwd.getText().toString());
+//                        SPUtils.getInstance().put(CommonParams.SP_PWD, GostDesBasae64Encrypter.encode(ed_username.getText().toString(), FusionField.key32));
                         // 登录成功跳转首页
                         Intent intent2 = new Intent(SignInActivity.this, SplashActivity.class);
                         intent2.putExtra(CommonParams.TYPE, CommonParams.MAIN);
@@ -109,9 +110,11 @@ public class SignInActivity extends BaseIMActivity {
                     // 删除旧数据
                     PlainTextDBHelper.getInstance(Utils.getApp()).clearAllInfo();
 
+//                    MTService.reqLogin(getApplicationContext(), ed_username.getText().toString(),
+//                            GostDesBasae64Encrypter.encode(ed_username.getText().toString(), FusionField.key32));
+
                     MTService.reqLogin(getApplicationContext(), ed_username.getText().toString(),
-                            GostDesBasae64Encrypter.encode(ed_username.getText().toString(), FusionField.key32)
-                    );
+                            ed_pwd.getText().toString());
                 }
                 break;
         }
